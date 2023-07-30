a333c6a738785270d96908cc2aa385e883be458e
Zeynab Khayyati <136909591+zeynabkhayyati@users.noreply.github.com>
2023-07-30 11:41:43 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,80 @@
+import pyautogui
+from time import sleep
+list = [
+
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-01-intro.mp4?h=_mYdrKHMyXOmLi8fzjKaBQ&t=1690376283',
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-02-git-vs-github-vs-gitlab.mp4?h=Au2TsA5iELamqgBV9oXbTg&t=1690376283',
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-03-I-love-git.mp4?h=TfdWl75nY77RMzbERH2nWA&t=1690376283',
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-04-when-must-learn-git.mp4?h=hxQ94cHbFXgOixFW6PQw-g&t=1690376283',
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-05-git-main-steps.mp4?h=bTtb2p-ynBxPi9x9euVk8g&t=1690376283',
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-06-install-git.mp4?h=qK_HqiEPaL6dZp_Ek24wYw&t=1690376283',
+'https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-07-git-init.mp4?h=7BqufbWLgOLrejdZRD_XJw&t=1690376283',
+
+]
+
+for i in range(len(list)):
+    sleep(0.01)
+    pyautogui.click(152 , 724)
+    pyautogui.write(list[i] , interval=0.001 )
+    pyautogui.press("enter")
+
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-01-intro.mp4?h=_mYdrKHMyXOmLi8fzjKaBQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-02-git-vs-github-vs-gitlab.mp4?h=Au2TsA5iELamqgBV9oXbTg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-03-I-love-git.mp4?h=TfdWl75nY77RMzbERH2nWA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-04-when-must-learn-git.mp4?h=hxQ94cHbFXgOixFW6PQw-g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-05-git-main-steps.mp4?h=bTtb2p-ynBxPi9x9euVk8g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-06-install-git.mp4?h=qK_HqiEPaL6dZp_Ek24wYw&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-07-git-init.mp4?h=7BqufbWLgOLrejdZRD_XJw&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-08-add-file-to-stage.mp4?h=xlq4-zEFu3satQZNouxoBQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-09-remove-file-from-stage.mp4?h=ZRWG9LAfPl2NsPG-cW1oLQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-10-git-commit.mp4?h=aGJ4hwVO_7pcBEeXODQHbA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-11-git-log.mp4?h=R1_hW5RGnmq_lpmYjRSLEQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-12-git-log-flags.mp4?h=7HsQV62-uknBeIwjn40A-g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-13-am-commit-flag.mp4?h=FojdDF9Zj-xqkc7LorDnjA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-14-git-show.mp4?h=KPZYp637JSbdNE94NQnGeQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-15-git-alias.mp4?h=vloT71FbaEngpEto76lazg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-16-what-is-branch.mp4?h=kh4X0FvWrVdc8lCHOeb4kg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-17-create-and-switch-on-branch.mp4?h=Povao1kE44lJcIMdrLTyUA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-18-remove-branch.mp4?h=iFFqHlUnzJAxJfIjrGx5BQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-19-rename-branch.mp4?h=jPVoUoaqT34y1c8Yj9OT7g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-20-gitlens-extension.mp4?h=Pb8_NrauipX-JWHfccSImQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-21-git-fast-forward-merge.mp4?h=Hk7m1GQTfca43y5BJb9dCA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-22-git-non-fast-forward-merge-01.mp4?h=bMecoSc0GpOdpNktLtYIfg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-23-git-non-fast-forward-merge-02.mp4?h=cHdpANqOJhaoowCTbVTkCQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-24-merge-best-practice.mp4?h=5_Xa0U4UODwz5pMue5WgoQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-25-semantic-commit-messages.mp4?h=WIkqK3RPrylQQFt2Zwe9_g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-26-git-diff.mp4?h=I3_QnhHyIDmElXRm8zrS6g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-27-git-diff-stage.mp4?h=AnKesgzyODmUZosDlmt6rQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-28-git-diff-head.mp4?h=CTio4Aj_k7-vIo33UbML8w&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-29-compare-commits-or-branches.mp4?h=p0o93BvujjgqkTazepELtg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-30-gitignore.mp4?h=k86uwsv_OzKOQZSFrK6GDA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-31-checkout.mp4?h=sp66xA0TwTqQwsTEKZQJIg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-32-discard-wd-changes.mp4?h=Ermfc2Dml3udenD9wDv5-A&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-33-git-restore-01.mp4?h=_RpT62rCWZwNHlbIeVX2ew&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-34-git-restore-02.mp4?h=jZYHuUPRxXFhXA6dfTv2uw&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-35-git-clean.mp4?h=d0stoWQOXVasrfZUbkvV9w&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-36-git-reset-concept.mp4?h=CnLLFVkODI8KFSrXVWB7vA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-37-soft-reset.mp4?h=DKxvSjTd1riY4uMAAQc-GQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-38-def-and-hard-reset.mp4?h=iVi2OSBtJR-qsr9hiHBmvw&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-39-revert-in-git.mp4?h=f1G9iolGrg8UQeLvjM3NLQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-40-conflict-in-revert.mp4?h=0B0QEJhLBNjm1NQ6wdyULw&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-41-git-status-short.mp4?h=ADZwnMdX-9YtZv22LuwqQQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-42-github-intro.mp4?h=S9wM6Shw9wACC0KlIu6A2g&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-43-create-github-acc.mp4?h=nfukUTIPywo2XQS-IJadkQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-44-git-clone.mp4?h=MEdZsQCm_JXKClgrUSEcPg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-45-create-repo-in-github.mp4?h=bIa97zuwPgxtNU53EdrD5Q&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-46-ways-to-connect-project-to-github.mp4?h=bNfCKMQxItIjRb4211VHgQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-47-git-remote.mp4?h=2JtB6jclWtjNoz7xVHSwbQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-48-git-push.mp4?h=vg5mXk7VKNeaUCSNet0RyQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-49-git-pull.mp4?h=dClxybE_XEk2aQPD6rFLvQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-50-git-fetch.mp4?h=0VC6k1gLAZHjAkPB-59H0A&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-51-remote-branch.mp4?h=p4t3x53tdSZIe5IacurjQA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-52-learn-deeply.mp4?h=_in-qSID4jWn5BxkbedBQA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-53-pull-with-conflict.mp4?h=Hs1KKJM52sxTVkm9PR0nXQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-54-readme-and-markdown.mp4?h=lvVnqjyDqEU9IkmHeAxQTQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-55-link-and-img-in-readme.mp4?h=_E_KF31iRAumNhl_w8A5xQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-56-badge-in-readme.mp4?h=1fpXSwB72JDaE1v0NeDIgA&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-57-readme-so.mp4?h=J8LeazFF0stTysSEgFhqxg&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-58-github-page.mp4?h=udl3h-BLMvt6sBRSHA4GZQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-59-private-repo.mp4?h=W_XU97zdSTo4HujM2mLlKQ&t=1690376283
+# https://tech.sabzlearn.ir/uploads/ce01010101it/git-github/Git-60-invite-collaborator.mp4?h=WXoZsws6ESdJRFMkxc8_gg&t=1690376283
